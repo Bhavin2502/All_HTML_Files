75ceb633c1104ce7ec5ad29dc804866bd37d4333
Bhavin2502 <daxinibhavin03@gmail.com>
2023-08-09 21:26:09 -0700

    All_Files

---
+++ personal information.cc++.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("Personal Information\n");
+	printf("Name:Bhavin daxini\n");
+	printf("Mobile No: 799015672\n");
+	printf("Adrress:Dhrangadhra\n");
+	return 0;
+	
+}
