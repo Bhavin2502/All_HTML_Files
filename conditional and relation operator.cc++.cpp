a99800ec8e07ab83894f10806b2439aaff69e159
Bhavin2502 <daxinibhavin03@gmail.com>
2023-08-14 21:35:01 -0700

    All_Files

---
+++ conditional and relation operator.cc++.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	// int x=10;
+//	  printf("%d",x==y);
+//	  printf("%d",x>y);
+//	  printf("%d",x<y);
+//	  printf("%d",x>=y);
+//	  printf("%d",x!=y);
+//    LOGICAL AND
+      int x=10;
+      //printf("%d",x>5 && x<20);
+      //pritf("%d",x>20 || x<40);
+      printf("%d",!x>5 && x<30);
+}
