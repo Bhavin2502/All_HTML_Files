911b6c2988996e8292b48dc5daeffd91bd3567bf
Bhavin2502 <daxinibhavin03@gmail.com>
2023-08-16 21:56:39 -0700

    All_Files

---
+++ square.cc++.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int b,h,a;
+	printf("enter lenght:");
+	scanf("%d",&b);
+	printf("enter width:");
+	scanf("%d",&h);
+	a=b*h/2;
+	printf("area is%d",a);
+	return 0;
+}
