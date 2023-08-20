052aeeb9a6873e81a929cf31253d6866ee474cc7
Bhavin2502 <daxinibhavin03@gmail.com>
2023-08-19 21:37:10 -0700

    All_Files

---
+++ else if.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+	int num;
+	printf("enter a nuber");
+	scanf("%d",&num);
+	if(num<0);
+	{
+		printf("nagative number");
+	}
+	else if(num>0);
+	{
+		printf("positive number");
+	}
+	else
+	{
+		printf("number is zero");
+	}
+}
