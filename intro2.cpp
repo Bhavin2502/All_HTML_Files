e7f61c459289c1ee709a8eeae923da761e3c72c4
Bhavin2502 <daxinibhavin03@gmail.com>
2023-09-15 20:53:54 -0700

    All_Files

---
+++ intro2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int age;
+	cout<<"enter the age :";
+    cin>>age;
+    if(age>18)
+    {
+    	cout<<"you can marry";
+    	
+	}
+	else
+	{
+		cout<<"child marriage is not allowed";
+	}
+}
